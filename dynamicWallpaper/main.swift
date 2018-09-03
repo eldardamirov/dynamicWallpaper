b54c5336df6625bd940a40b410d2ab615c22f32f
eldardamirov <eldardamirov75@mail.ru>
2018-09-03 22:09:20 +0300

    Initial Commit

---
+++ dynamicWallpaper/main.swift
@@ -0,0 +1,52 @@
+//
+//  main.swift
+//  dynamicWallpaper
+//
+//  Created by Эльдар Дамиров on 03.09.2018.
+//  Copyright © 2018 Эльдар Дамиров. All rights reserved.
+//
+
+import AppKit
+
+func setWallpaper(wallpaperPath: String)
+    {
+    var error : NSError?;
+    let sharedWorkspace = NSWorkspace.shared;
+    let screens = NSScreen.screens;
+    let wallpaperUrl : NSURL = NSURL.fileURL(withPath: wallpaperPath) as NSURL;
+
+    
+    guard var options = sharedWorkspace.desktopImageOptions(for: screens [ 0 ] ) else 
+        {
+        return;
+        }
+
+        options[NSWorkspace.DesktopImageOptionKey.imageScaling] = 1;
+        options[NSWorkspace.DesktopImageOptionKey.allowClipping] = true;
+
+
+    for screen in screens
+        {
+        do 
+            {
+            try sharedWorkspace.setDesktopImageURL ( wallpaperUrl as URL, for: screen, options: options );
+            }
+        catch 
+            {
+            print ( error.localizedDescription );
+            }
+        }
+    }
+
+
+
+var wallpaperExtension = ".jpeg";
+
+for index in 1...16
+    {
+    Thread.sleep ( until: Date ( timeIntervalSinceNow: 0.5 ) )
+    setWallpaper ( wallpaperPath: String ( String ( index ) + wallpaperExtension ) );
+    }
+
+
+
