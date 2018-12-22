ed28df852775112526fce47b7a2787cdd5bd5b99
eldardamirov <eldardamirov75@mail.ru>
2018-12-23 01:39:11 +0300

    Finished auto image change.

--- dynamicWallpaper/main.swift
+++ dynamicWallpaper/main.swift
@@ -10,9 +10,12 @@
 
 import AppKit
 
+
+
+
 let wallpaperExtension = ".jpeg";
 let lk = LightKit();
-let time = NSDate();
+var lightSensorsReadings = lk?.lightSensors;
 
 func setWallpaper(wallpaperPath: String)
     {
@@ -44,21 +47,64 @@ func setWallpaper(wallpaperPath: String)
         }
     }
     
-//func determineIndex ( light: Int ) -> String
-//    {
-//    
-//    }
+func determineIndex() -> String
+    {
+    let time = NSDate();
+    var index: Int!;
+    
+    switch time.currentHour()
+        {
+        case 5...6:
+            index = 1;
+        case 7:
+            index = 2;
+        case 8:
+            index = 3;
+        case 9...10:
+            index = 4;
+        case 11:
+            index = 5;
+        case 12...13:
+            index = 6;
+        case 14:
+            index = 7;
+        case 15...16:
+            index = 8;
+        case 17:
+            index = 9;
+        case 18:
+            index = 10;
+        case 19:
+            index = 11;
+        case 20:
+            index = 12;
+        case 21...22:
+            index = 13;
+        case 0...1, 23:
+            index = 14;
+        case 2:
+            index = 15;
+        case 3...4:
+            index = 16;
+        
+        default:
+            index = 16;
+        }
+        
+    return String ( String ( index ) + wallpaperExtension );
+    }
 
-print ( "Current time hour: \( time.currentMinute() )" );
 
 
 
-var lightSensorsReadings = lk?.lightSensors;
+
+
 
 while ( true )
     {
-    Thread.sleep ( until: Date ( timeIntervalSinceNow: 60 ) );
-    lightSensorsReadings = lk?.lightSensors;
+    Thread.sleep ( until: Date ( timeIntervalSinceNow: 1 ) );
+//    lightSensorsReadings = lk?.lightSensors;
+    setWallpaper ( wallpaperPath: determineIndex() );
     }
 
 
